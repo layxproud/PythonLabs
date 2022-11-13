d976d6be13ad326d098f8cc1f10ffd0f5ca1ad9c
LayxProud <layxproud@mail.ru>
2022-11-13 17:24:09 +0300

    Initial commit

---
+++ .venv/lab3.py
@@ -0,0 +1,6 @@
+letters = ['a', 'b', 'c']
+
+def my_enumerate(my_list):
+    return list(zip(range(len(my_list)), my_list)) 
+    
+print(my_enumerate(letters))
---
+++ .venv/lab7.py
@@ -0,0 +1,59 @@
+import math
+
+class MyVector:
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y
+        
+    def __repr__(self):
+        return f"<{self.x}; {self.y}>"
+    
+    def __str__(self):
+        return f"<{self.x}; {self.y}>"   
+    
+    def __add__(self, other):
+        print(MyVector(self.x + other.x, self.y + other.y))
+    
+    def __eq__(self, other):
+        if self.__abs__() == other.__abs__():
+            print("YES")
+        else:
+            print("NO")
+        
+    def __mul__(self, other):
+        if type(other) is MyVector:
+            print(MyVector(self.x * other.x, self.y * other.y))
+        else :
+            print(MyVector(self.x * other, self.y * other))
+            
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+#-----------------------------------------------------------    
+def main():
+    vector1 = MyVector(1, 2)
+    vector2 = MyVector(3, 4)
+    
+    print("First vector:", end = " ")
+    print(vector1)
+    
+    print("Second vector:", end = " ")
+    print(vector2)
+    
+    print(f"Vector {vector1} + vector {vector2} = ", end = " ")
+    vector1 + vector2
+    
+    print(f"Is vector {vector1} equal to vector {vector2}?", end = " ")
+    vector1 == vector2
+    
+    print(f"Vector {vector1} * 3 = ", end = " ")
+    vector1 * 3
+    
+    print(f"Vector {vector1} * vector {vector2} =", end = " ")
+    vector1 * vector2
+    
+    print(f"Length of vector {vector1} is", end = " ")
+    print(vector1.__abs__())
+#-----------------------------------------------------------    
+if __name__ == "__main__":
+    main()
+        
