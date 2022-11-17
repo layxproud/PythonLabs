b61607f9cc25ead1b23cad5aa928d8093f84d72d
LayxProud <49621292+LayxProud@users.noreply.github.com>
2022-11-17 17:00:28 +0300

    Delete .venv directory

--- .venv/lab2.py
+++
@@ -1,7 +0,0 @@
-list = [1, 2, 1, 3, 2, 5]
-
-def unique(list):
-    newlist = set(list)
-    return newlist
-
-print(unique(list))
--- .venv/lab3.py
+++
@@ -1,6 +0,0 @@
-letters = ['a', 'b', 'c']
-
-def my_enumerate(my_list):
-    return list(zip(range(len(my_list)), my_list)) 
-    
-print(my_enumerate(letters))
