6fec597e10904dc84ea1441d54b05c0865bf2d6d
LayxProud <layxproud@mail.ru>
2022-12-15 17:04:07 +0300

    Changed realization of decorator class

--- lab9.py
+++ lab9.py
@@ -1,65 +1,52 @@
 import time
 
 
-def timer(func):
-    def wrapper(*args, **kw):
-        start_time = time.time()
-        result = func(*args, **kw)
-        end_time = time.time()
-        run_time = end_time - start_time
-        print(f"Finished {func.__name__} in {run_time:.4f} secs")
-        return result
-    return wrapper
+class Decorator:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, args):
+        date = time.strftime("%H:%M:%S", time.localtime())
+        print(f'<{date}>: function {self.func.__name__} '
+              'called with arguments {args}')
+        start = time.time()
+        self.func(args)
+        end = time.time()
+        diff = f"{end-start:01f}"
+        print(f"Class-Decorator elapsed time = {diff} sec")
+        with open(f"{self.func.__name__}.html", 'a', encoding='utf-8') as f:
+            f.write(f"<html><body>Время: {diff} секунд</body></html>\n")
+        f.close()
 
 
-def timer_all_methods(cls):
-    class NewCls:
-        def __init__(self, *args, **kwargs):
-            self._obj = cls(*args, **kwargs)
-
-        def __getattribute__(self, s):
-            try:
-                x = super().__getattribute__(s)
-            except AttributeError:
-                pass
-            else:
-                return x
-            attr = self._obj.__getattribute__(s)
-            if isinstance(attr, type(self.__init__)):
-                return timer(attr)
-            else:
-                return attr
-    return NewCls
+def gen_list(n):
+    lst = list()
+    for num in range(1, n):
+        lst.append(num)
+    return lst
 
 
-@timer_all_methods
-class Squares:
-    def gen_list(self, n):
-        lst = list()
-        for num in range(1, n):
-            lst.append(num)
-        return lst
+@Decorator
+def squares_for(lst: list) -> list:
+    for i in range(len(lst)):
+        lst[i] **= 2
 
-    def squares_for(self, lst: list) -> list:
-        for i in range(len(lst)):
-            lst[i] **= 2
-        return lst
 
-    def squares_list_comprehenshion(self, lst: list) -> list:
-        lst = [i ** 2 for i in lst]
-        return lst
+@Decorator
+def squares_list_comprehenshion(lst: list) -> list:
+    lst = [i ** 2 for i in lst]
 
-    def squares_map(self, lst: list) -> list:
-        lst = list(map(lambda x: x**2, lst))
-        return lst
+
+@Decorator
+def squares_map(lst: list) -> list:
+    lst = list(map(lambda x: x**2, lst))
 
 
 def main():
-    f = Squares()
-    lst = f.gen_list(10000000)
-    f.squares_for(lst)
-    f.squares_list_comprehenshion(lst)
-    f.squares_map(lst)
+    lst = gen_list(10000000)
+    squares_for(lst)
+    squares_list_comprehenshion(lst)
+    squares_map(lst)
 
 
 if __name__ == "__main__":
